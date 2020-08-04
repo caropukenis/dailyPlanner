f4850fee2612218d3121b80f2337cf5f78454628
Caroline Pukenis <caro_pukenis@yahoo.com>
2020-08-04 17:05:37 -0500

    pushingFiles

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
+
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "23 3*/-*4 n-----4§/moment/moment.min.js";
+
+let m = moment();
+
+console.log(m);
