32e6a5e4d65fc21ebb5278104ebd83ae7755c0b6
Caroline Pukenis <caro_pukenis@yahoo.com>
2020-08-05 17:48:51 -0500

    update4

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,145 @@
-var script = document.createElement("script");
-script.type = "text/javascript";
-script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
+$(currentDay).text(moment().format("MMMM Do, YYYY"));
 
-var script = document.createElement("script");
-script.type = "text/javascript";
-script.src = "23 3*/-*4 n-----4§/moment/moment.min.js";
+let planner = [
+  {
+    id: "0",
+    hour: "9",
+    time: "09",
+    meridiem: "am",
+    reminder: "",
+  },
+  {
+    id: "1",
+    hour: "10",
+    time: "10",
+    meridiem: "am",
+    reminder: "",
+  },
+  {
+    id: "2",
+    hour: "11",
+    time: "11",
+    meridiem: "am",
+    reminder: "",
+  },
+  {
+    id: "3",
+    hour: "12",
+    time: "12",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "4",
+    hour: "1",
+    time: "13",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "5",
+    hour: "2",
+    time: "14",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "6",
+    hour: "3",
+    time: "15",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "7",
+    hour: "4",
+    time: "16",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "8",
+    hour: "5",
+    time: "17",
+    meridiem: "pm",
+    reminder: "",
+  },
+];
 
-let m = moment();
+function saveReminders() {
+  localStorage.setItem("planner", JSON.stringify(planner));
+}
 
-console.log(m);
+function displayReminders() {
+  planner.forEach(function (_thisHour) {
+    $(`#${_thisHour.id}`).val(_thisHour.reminder);
+  });
+}
+
+function startPlanner() {
+  let storedDay = JSON.parse(localStorage.getItem("planner"));
+
+  if (storedDay) {
+    planner = storedDay;
+  }
+  saveReminders();
+  displayReminders();
+}
+
+planner.forEach(function (thisHour) {
+  let hourRow = $("<form>").attr({
+    class: "row",
+  });
+  $(".container").append(hourRow);
+
+  let hourField = $("<div>").text(`${thisHour.hour}${thisHour.meridiem}`).attr({
+    class: "col-md-2 hour",
+  });
+
+  let hourPlan = $("<div>").attr({
+    class: "col-md-9 description p-0",
+  });
+
+  let planData = $("<textarea>");
+  hourPlan.append(planData);
+  planData.attr("id", thisHour.id);
+  if (thisHour.time < moment().format("HH")) {
+    planData.attr({
+      class: "past",
+    });
+  } else if (thisHour.time === moment().format("HH")) {
+    planData.attr({
+      class: "present",
+    });
+  } else if (thisHour.time > moment().format("HH")) {
+    planData.attr({
+      class: "future",
+    });
+  }
+
+  // creates save button using save icon from font awesome
+  let saveButton = $("<i class='far fa-save fa-lg'></i>");
+  let savePlan = $("<button>").attr({
+    class: "col-md-1 saveBtn",
+  });
+  savePlan.append(saveButton);
+  hourRow.append(hourField, hourPlan, savePlan);
+});
+
+// loads any existing localstorage data after components created
+startPlanner();
+
+// saves data to be used in localStorage
+$(".saveBtn").on("click", function (event) {
+  event.preventDefault();
+  let saveIndex = $(this)
+    .siblings(".description")
+    .children(".future")
+    .attr("id");
+  planner[saveIndex].reminder = $(this)
+    .siblings(".description")
+    .children(".future")
+    .val();
+  saveReminders();
+  displayReminders();
+});
